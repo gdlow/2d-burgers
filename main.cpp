63eb3548d4812de1e7d35d16181de498e8dc78f2
Gerald Low <gerald.low@skyscanner.net>
2019-02-24 10:10:04 +0000

    Moved typedef to top of main

--- main.cpp
+++ main.cpp
@@ -7,13 +7,14 @@
 int main(int argc, char* argv[]) {
     Model m(argc, argv);
 
+    // typedef std::chrono::high_resolution_clock hrc;
+    // typedef std::chrono::milliseconds ms;
+
     if (argc == 9 && atoi(argv[8]) == 1) {
         Burgers2P b(m);
         // Call code to initialise the problem here;
         m.PrintParameters();
 
-        // typedef std::chrono::high_resolution_clock hrc;
-        // typedef std::chrono::milliseconds ms;
         // hrc::time_point start = hrc::now();
 
         // Call code to perform time integration here
@@ -30,8 +31,6 @@ int main(int argc, char* argv[]) {
         // Call code to initialise the problem here;
         m.PrintParameters();
 
-        // typedef std::chrono::high_resolution_clock hrc;
-        // typedef std::chrono::milliseconds ms;
         // hrc::time_point start = hrc::now();
 
         // Call code to perform time integration here
