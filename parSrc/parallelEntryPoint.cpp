905801d80577addeca73e968a97988d4efaf5b32
Gerald Low <gerald.low@skyscanner.net>
2019-02-27 18:50:38 +0000

    Added time recordings

--- parSrc/parallelEntryPoint.cpp
+++ parSrc/parallelEntryPoint.cpp
@@ -1,23 +1,30 @@
 #include <chrono>
 #include "Model2P.h"
 #include "Burgers2P.h"
+#include <iostream>
 
 int main(int argc, char* argv[]) {
     Model m(argc, argv);
 
-    // typedef std::chrono::high_resolution_clock hrc;
-    // typedef std::chrono::milliseconds ms;
+    typedef std::chrono::high_resolution_clock hrc;
+    typedef std::chrono::milliseconds ms;
+    typedef std::chrono::duration<double> fsec;
+
     Burgers2P b(m);
     // Call code to initialise the problem here;
     m.PrintParameters();
 
-    // hrc::time_point start = hrc::now();
+    hrc::time_point start = hrc::now();
 
     // Call code to perform time integration here
     b.SetInitialVelocity();
     b.SetIntegratedVelocity();
 
-    // hrc::time_point end = hrc::now();
+    hrc::time_point end = hrc::now();
+    fsec elapsed_seconds = end-start;
+    ms elapsed_ms = std::chrono::duration_cast<ms>(elapsed_seconds);
+    std::cout << "Time elapsed: " << elapsed_seconds.count() << " s" << std::endl;
+    std::cout << "Time elapsed: " << elapsed_ms.count() << " ms" << std::endl;
 
     // Calculate final energy and write output
     b.SetEnergy();
