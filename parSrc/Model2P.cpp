e9f873007d1c2420063a080213b1baabc7d5d197
Gerald Low <gerald.low@skyscanner.net>
2019-03-01 20:11:01 +0000

    Modify plotData.m for plotting

--- parSrc/Model2P.cpp
+++ parSrc/Model2P.cpp
@@ -96,9 +96,9 @@ void Model::ValidateParameters() {
  * @brief Set appropriate values for various members
  * */
 void Model::SetNumerics() {
-    Nx = 2001;
-    Ny = 2001;
-    Nt = 4001;
+    Nx = 101;
+    Ny = 101;
+    Nt = 101;
     // dx,dy and dt are dependent on L,T and Nx,Ny,Nt:
     dx = Lx / (Nx-1);
     dy = Ly / (Ny-1);
--- plotData.m
+++ plotData.m
@@ -1,32 +1,25 @@
 clear; clc; close all;
 
 %% input params
-Nt = 101;
-Nx = 101;
-Ny = 101;
+Nx = 2001;
+Ny = 2001;
 Lx = 10;
 Ly = 10;
 T = 1;
 %% read data
-filePath = 'data.txt';
+filePath = 'data_report_4001.txt';
 fileID = fopen(filePath,'r');
 fgets(fileID);
-U = zeros(Nt,Ny,Nx);
-for k=1:Nt
-    fgets(fileID);
-    for j=1:Ny
-        tline = fgets(fileID);
-        U(k,j,:) = cell2mat(textscan(tline,'%f'))';
-    end
-end
-fgets(fileID);
-V = zeros(Nt,Ny,Nx);
-for k=1:Nt
+U = zeros(Ny,Nx);
+for j=1:Ny
     tline = fgets(fileID);
-    for j=1:Ny
-        tline = fgets(fileID);
-        V(k,j,:) = cell2mat(textscan(tline,'%f'))';
-    end
+    U(j,:) = cell2mat(textscan(tline,'%f'))';
+end
+V = zeros(Ny,Nx);
+fgets(fileID);
+for j=1:Ny
+    tline = fgets(fileID);
+    V(j,:) = cell2mat(textscan(tline,'%f'))';
 end
 fclose(fileID);
 
@@ -35,17 +28,13 @@ fclose(fileID);
 x = linspace(-Lx/2,Lx/2, Nx);
 y = linspace(-Ly/2,Ly/2, Ny);
 [X,Y] = meshgrid(x,y);
+
+% U
 figure;
-zlim manual;
-for k=1:Nt
-    mesh(X, Y, squeeze(U(k,:,:)));
-    zlim([0 2]);
-    pause(0.01);
-end
+mesh(X, Y, U);
+title('U velocity field at T=1');
+
+% V
 figure;
-zlim manual
-for k=1:Nt
-    mesh(X, Y, squeeze(V(k,:,:)));
-    zlim([0 2]);
-    pause(0.01);
-end
+mesh(X, Y, V);
+title('V velocity field at T=1');
